c78df4b1c2224ab985e1d9cd4058a9ba3af7e7e2
gy0318 <372301467@qq.com>
2020-05-05 15:21:43 +0800

    init

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  div
+    div {{haha}}
+    div {{haha | formatDate}}
+    div {{haha | haha}}
+    div {{heihei | go}}
+</template>
+
+<script>
+import Vue from 'vue'
+
+Vue.filter( 'formatDate', value => {
+  return new Date(value)
+})
+
+Vue.filter( 'haha', value => {
+  return 999
+})
+
+export default {
+  data(){
+    return {
+      haha: '2018-10-29',
+      heihei: 'nihao'
+    }
+  }
+}
+</script>
